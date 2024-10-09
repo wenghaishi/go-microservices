5edd525cd760738a98af99015f5aa1543b62c65b
weng haishi <wenghaishi1@gmail.com>
2024-10-10 00:55:34 +0800

    create routes.go

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {}
+
 func main () {
-	
+	app := Config{}
+
+	log.Println("Starting service on port %s", webPort)
+
+	// define http server
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s")
+	}
+
+
 }
